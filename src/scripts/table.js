b8df75e3bc23a63cf984e0adf1aff8845cbe13a8
David <cheva79@gmail.com>
2023-08-12 18:51:02 -0500

    table.js updated

--- src/scripts/table.js
+++ src/scripts/table.js
@@ -1,27 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let selectOption = document.getElementById('SelectJornada')
+    let ButtonJornada = document.getElementById("ButtonJornada");
+    console.log('SelectOption:',selectOption.value)
 
-let selectOption = document.getElementById('SelectJornada')
 
-let ButtonJornada = document.getElementById("ButtonJornada");
-console.log('SelectOption:',selectOption.value)
+    ButtonJornada.addEventListener("click", async () => {
+        let file = selectOption.value 
 
-ButtonJornada.addEventListener("click",async() => {
-    let file = selectOption.value 
-    await fetch('../datatable/La'+file+'.json') 
-    .then((response) => response.json())
-    .then((json) => {
-        var table = document.getElementById("data-output")  
-        table.innerHTML =''   
-        for (let i = 0; i < 10; i++){
-            var row = `<tr>
-                            <td>${json.Fecha[i]}</td>
-                            <td>${json.Hora[i]}</td>
-                            <td>${json.ResultadoLocal[i]}</td>
-                            <td>Vs</td>
-                            <td>${json.ResultadoVisitante[i]}</td>
-                            <td>${json.EquipoLocal[i]}</td>
-                            <td>${json.EquipoVisitante[i]}</td>
-                        </tr>`
-            table.innerHTML += row}
-        })
-    })
+        const data = await fetch('../src/datatable/La'+file+'.json').then((response) =>
+            response.json())
+            console.log(data);
+            // .then((json) => {
+                var table = document.getElementById("data-output")  
+                table.innerHTML =''   
+                for (let i = 0; i < 10; i++){
+                    var row = `<tr>
+                                    <td>${data.Fecha[i]}</td>
+                                    <td>${data.Hora[i]}</td>
+                                    <td>${data.ResultadoLocal[i]}</td>
+                                    <td>Vs</td>
+                                    <td>${data.ResultadoVisitante[i]}</td>
+                                    <td>${data.EquipoLocal[i]}</td>
+                                    <td>${data.EquipoVisitante[i]}</td>
+                                </tr>`
+                    table.innerHTML += row}
+            })
+        // })
+})
 
+// D:\Personal\Astro_JS\tremendous-trappist\src\scripts\table.js
+// D:\Personal\Astro_JS\tremendous-trappist\src\datatable\LaJornada2.json
